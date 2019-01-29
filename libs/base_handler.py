70514194b99fc946e25458dba1a733fce87c7609
shenshuo <shenshuo@shinezone.com>
2019-01-29 14:35:51 +0800

    权限使用

--- libs/base_handler.py
+++ libs/base_handler.py
@@ -18,21 +18,19 @@ class BaseHandler(SDKBaseHandler):
 
     def prepare(self):
         ### 登陆验证
-        # auth_key = self.get_cookie('auth_key', None)
-        # if not auth_key:
-        #     # 没登录，就让跳到登陆页面
-        #     raise HTTPError(401, 'auth failed 1')
-        #
-        # else:
-        #     user_info = jwt.decode(auth_key, verify=False).get('data')
-        #     self.user_id = user_info.get('user_id', None)
-        #     self.username = user_info.get('username', None)
-        #     self.nickname = user_info.get('nickname', None)
-        #     self.is_super = user_info.get('is_superuser', False)
-        #
-        #     if not self.user_id:
-        #         raise HTTPError(401, 'auth failed 2')
+        auth_key = self.get_cookie('auth_key', None)
+        if not auth_key:
+            # 没登录，就让跳到登陆页面
+            raise HTTPError(401, 'auth failed 1')
 
-        #self.is_superuser = self.is_super
-        self.nickname = '杨铭威'
-        #self.is_superuser = True
+        else:
+            user_info = jwt.decode(auth_key, verify=False).get('data')
+            self.user_id = user_info.get('user_id', None)
+            self.username = user_info.get('username', None)
+            self.nickname = user_info.get('nickname', None)
+            self.is_super = user_info.get('is_superuser', False)
+
+            if not self.user_id:
+                raise HTTPError(401, 'auth failed 2')
+
+        self.is_superuser = self.is_super
