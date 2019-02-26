30abff06597edee7165d0e9abef718aa92fc4e9f
shenshuo <shenshuo@shinezone.com>
2019-02-26 15:30:44 +0800

    更新获取配置脚本

--- libs/get_config.py
+++ libs/get_config.py
@@ -11,7 +11,6 @@ Desc    : 获取配置文件内容，
 
 import os
 import requests
-import pyotp
 import json
 
 
@@ -19,21 +18,16 @@ class ConfApi:
     def __init__(self):
         self.user = 'publish'
         self.pwd = 'shenshuo'
-        self.key = 'JJFTQNLXHBYWSVSCINNEWNDFJRKWGY2UNJTTSVTLMN3UGUKXPFDE4NDH'
         self.login_api = 'http://gw.shinezone.net.cn/api/accounts/login/'
         self.conf_path ='/tmp'
         self.conf_config_api = "http://gw.shinezone.net.cn/api/kerrigan/v1/conf/publish/config/"
         self.conf_service_api = "http://gw.shinezone.net.cn/api/kerrigan/v1/conf/publish/service/"
 
-    @property
-    def get_mfa(self):
-        t = pyotp.TOTP(self.key)
-        return t.now()
 
     def login(self):
         try:
             headers = {"Content-Type": "application/json"}
-            params = {"username": self.user, "password": self.pwd, "dynamic": self.get_mfa}
+            params = {"username": self.user, "password": self.pwd}
             result = requests.post(self.login_api, data=json.dumps(params), headers=headers)
 
             ret = json.loads(result.text)
@@ -86,6 +80,23 @@ class ConfApi:
             print('config file path is {}'.format(config_file))
         print('success')
 
+    def create_config_key(self, config_key):
+        ### 根据配置KEY 生成配置文件
+        config_list = os.path.split(config_key)
+        config_dir, filename = config_list[0],config_list[1]
+        _, project_code, environment, service = config_dir.split('/')
+
+        config_data = self.get_config_details(project_code, environment, service, filename)
+        for k,v in config_data.items():
+            config_file = self.conf_path + k
+            dir_name, _ = os.path.split(config_file)
+            if not os.path.exists(dir_name):
+                os.makedirs(dir_name)
+            with open(config_file ,'a+') as f:
+                f.write(v)
+            print('config file path is {}'.format(config_file))
+        print('success')
+
     def create_service_config_file(self, project_code, environment, service):
         # 生成服务所有配置文件
         config_data = self.get_service_details(project_code, environment, service)
@@ -104,4 +115,5 @@ if __name__ == '__main__':
     # print(obj.get_config_details('ss', 'dev', 'nginx', 'nginx.conf'))
     # print(obj.get_service_details('ss', 'dev', 'nginx'))
     obj.create_config_file('ss','dev','nginx','nginx.conf')
+    # obj.create_config_key('/kerrigan/dev/kubernetes/deployment.yaml')
     # obj.create_service_config_file('ss', 'dev', 'nginx')
