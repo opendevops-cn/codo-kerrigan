38255632c11a8d8349c909fb29ccee9327ffa9f2
shenshuo <191715030@qq.com>
2023-03-16 14:50:25 +0800

    2023年3月16日  优化一点点01

--- run-py.sh
+++ run-py.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 sleep 1
-SERVICE_NAME=`echo $1|cut -d- -f2-`
-cd /data && python3 startup.py --service=${SERVICE_NAME}
+SERVICE_NAME=$1
+cd /data && python3 db_sync.py && python3 startup.py --service="${SERVICE_NAME}"
