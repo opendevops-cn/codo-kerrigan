0e7ed3cc5e935c2fbc0ae17754b64265fe62d948
shenshuo <191715030@qq.com>
2023-03-16 13:57:44 +0800

    2023年3月16日  优化一点点01

--- startup.py
+++ startup.py
@@ -11,19 +11,20 @@ import fire
 from tornado.options import define
 from websdk2.program import MainProgram
 from settings import settings as app_settings
-from biz.applications import Application as config_api
+from biz.applications import Application as configApi
 
 define("service", default='api', help="start service flag", type=str)
-
 
 class MyProgram(MainProgram):
     def __init__(self, progress_id='config'):
         self.__app = None
         settings = app_settings
-        self.__app = config_api(**settings)
+        self.__app = configApi(**settings)
         super(MyProgram, self).__init__(progress_id)
         self.__app.start_server()
 
 
 if __name__ == '__main__':
     fire.Fire(MyProgram)
+
+### python3 startup.py --service=kerrigan --port=8030
